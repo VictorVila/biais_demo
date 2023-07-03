e09dc859c65f956741a816c7ee2a6cdff25dce7b
VictorVila <victor@tirop.com>
2023-07-03 23:00:31 +0200

    Added descriptions and notebook

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Biais démo
 
-Cette application a été conçue pour servir de support pour expliquer aux élèves de droit ce que c'est que le biais dû aux données d'entraintement.
+Application de support pour expliquer aux élèves de droit ce que c'est que le biais dû aux données d'entraînement.
 
 Live : https://huggingface.co/spaces/VictorVS/biais_demo 
 
 ## Scénario
 
-Une entreprise veut fluidifier son processus de recrutement et lance un projet d'IA qui trie des CVs. Pour cela, l'IA est entraînée avec les données issues des entretiens de recrutement précedents. L'idée subyacente est que l'IA sera capable de découvrir les critères de sélection partagés par les CV des personnes récrutées. Concrètement, chaque entretien est représenté comme une ligne d'un tableau qui comporte des informations des candidats ansi que l'issue de l'entretien. 
+Une entreprise veut fluidifier son processus de recrutement et lance un projet d'IA qui trie des CVs. Pour cela, l'IA est entraînée avec les données issues des entretiens de recrutement précédents. L'idée subjacente est que l'IA sera capable de découvrir les critères de sélection partagés par les CV des personnes recrutées. Concrètement, chaque entretien est représenté comme une ligne d'un tableau qui comporte des informations des candidats ainsi que l'issue de l'entretien. 
 
-Imaginons qu'un recrutement est lancé pour embaucher un profil "confirmé" et que les données d'entraînement présentent un biais évident : aucune femme n'a été retenue et tous les hommes ont été recrutés sans tenir compte de leur profil (junior, confirmé ou sénior). L'IA saura-t-elle préconiser les profils confirmés ? 
+Imaginons qu'un recrutement est lancé pour embaucher un profil "confirmé" et que les données d'entraînement présentent un biais évident : aucune femme n'a été retenue et tous les hommes ont été recrutés sans tenir compte de leur profil (junior, confirmé ou senior). L'IA saura-t-elle préconiser les profils confirmés ? 
 
 Pour le savoir il suffit de cliquer sur le bouton "soumettre" qui lance ces actions :
 
@@ -16,8 +16,8 @@ Pour le savoir il suffit de cliquer sur le bouton "soumettre" qui lance ces acti
 2. Utilisation du modèle pour réaliser des prédictions sur des données de test ;
 3. Calcul et affichage de l'importance accordée par le modèle à chaque colonne des données ;
 
-Les réultats sont ceux que vous attendiez ? 
+Les résultats sont ceux que vous attendiez ? 
 
-Sauriez-vous modifier les données d'entraînement pour éviter le biais et qu'il réussise à identifier les profils "confirmés" ? Essayez à le faire et cliquez à nouveau sur "Soumettre".  
+Sauriez-vous modifier les données d'entraînement pour éviter le biais et qu'il réussisse à identifier les profils "confirmés" ? Essayez à le faire et cliquez à nouveau sur "Soumettre".  
 
  
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import pandas as pd
+# Imports
 
+import pandas as pd
 import sklearn
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
@@ -10,16 +11,19 @@ from sklearn.pipeline import make_pipeline
 import gradio as gr
 
 
+# Presentation for Huggingface
 
-description = "Démo simple des biais"
-title = "Biais démo"
-examples = [["TODO."]]
+description = "Simple app for demonstration of data bias"
+title = "Bias demo" 
 
+
+# App
 
 def get_data_raw() :
+    """Returns the initial (and higly biased) train dataset""" 
     data = {
         'Titre' : ['Mme', 'Mme', 'Mme', 'M', 'M', 'M'],
-        'Nom' : ['Rachel', 'Phoebe', 'Monica', 'HChandler', 'Joey', 'Ross'],  
+        'Nom' : ['Rachel', 'Phoebe', 'Monica', 'Chandler', 'Joey', 'Ross'],  
         'Expérience' : ['Junior', 'Confirmé', 'Confirmé', 'Junior', 'Senior', 'Confirmé'],
         'Recrutement' : ['Non', 'Non', 'Non', 'Oui', 'Oui', 'Oui'],
     } 
@@ -28,6 +32,7 @@ def get_data_raw() :
 
 
 def get_data_test() :
+    """Returns the test dataset""" 
     data_test = {
         'Titre' : ['Mme', 'Mme', 'Mme', 'M', 'M', 'M'],
         'Nom' : ['Leia', 'Padme', 'Rei', 'Han Solo', 'Luke', 'Chewbacca'],  
@@ -37,6 +42,7 @@ def get_data_test() :
     return df_data_test
 
 def get_model(data, target) : 
+    """Trains a decision tree and returns the model and the classifier itself """ 
     categorical_columns_selector = selector(dtype_include=object) 
     categorical_columns = categorical_columns_selector(data) 
     categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")  
@@ -48,40 +54,54 @@ def get_model(data, target) :
     return pipeline.fit(data, target), decission_tree
 
 def get_features_importance(model, classifier): 
+    """Returns the weight of each feature in the classifier""" 
     df1 = pd.DataFrame(data=model[:-1].get_feature_names_out(), columns=['Critère'])
     df2 = pd.DataFrame(data=classifier.feature_importances_, columns=['Importance'])
     df_concat = pd.concat([df1, df2], axis=1)
     return df_concat 
 
-def get_output(data_raw):
-    
-    # Le nom n'est pas utile 
+def get_output(data_raw): 
+    """Applies the model to the test data and gets predictions.
+    Returns the test data along qith the predictions and the importance of each feature"""      
     data = data_raw.drop(columns=['Nom','Recrutement'])
-    target = data_raw.Recrutement
-    
-    model, decission_tree = get_model(data, target) 
-    
-    data_test = get_data_test()
-    
-    predictions = model.predict(data_test)
-    
+    target = data_raw.Recrutement 
+    model, decission_tree = get_model(data, target)  
+    data_test = get_data_test() 
+    predictions = model.predict(data_test) 
     result = pd.concat([data_test, pd.DataFrame(data = predictions, columns={'Predictions'})], axis = 1)
-    
     features_importance = get_features_importance(model, decission_tree)
-    
     return result, features_importance
 
 
+# Launching the Gradio UI 
+
 ui = gr.Interface(
     fn=get_output, 
-    inputs=[
-        gr.Dataframe(get_data_raw())
+    inputs=[ 
+        gr.Dataframe(get_data_raw(), label="Données d'entraînement")
     ],
     outputs=[
-        gr.Dataframe(),
-        gr.Dataframe()
+        gr.Dataframe(label="Prédictions"),
+        gr.Dataframe(label="Importance des paramètres")
     ],
-    description="Show and editable dataframe and use it as output "
-)
+    title="Biais démo",
+    description="""Application de support pour expliquer aux élèves de droit ce que c'est que le biais dû aux données d'entraînement.
 
+Live : https://huggingface.co/spaces/VictorVS/biais_demo 
+
+## Scénario
+
+Une entreprise veut fluidifier son processus de recrutement et lance un projet d'IA qui trie des CVs. Pour cela, l'IA est entraînée avec les données issues des entretiens de recrutement précédents. L'idée subjacente est que l'IA sera capable de découvrir les critères de sélection partagés par les CV des personnes recrutées. Concrètement, chaque entretien est représenté comme une ligne d'un tableau qui comporte des informations des candidats ainsi que l'issue de l'entretien. 
+
+Imaginons qu'un recrutement est lancé pour embaucher un profil "confirmé" et que les données d'entraînement présentent un biais évident : aucune femme n'a été retenue et tous les hommes ont été recrutés sans tenir compte de leur profil (junior, confirmé ou senior). L'IA saura-t-elle préconiser les profils confirmés ? 
+
+Pour le savoir il suffit de cliquer sur le bouton "soumettre" qui lance ces actions :
+
+1. Entraînement d'un modèle à partir de données fournies ;
+2. Utilisation du modèle pour réaliser des prédictions sur des données de test ;
+3. Calcul et affichage de l'importance accordée par le modèle à chaque colonne des données ;
+
+Les résultats sont ceux que vous attendiez ? 
+
+Sauriez-vous modifier les données d'entraînement pour éviter le biais et qu'il réussisse à identifier les profils "confirmés" ? Essayez à le faire et cliquez à nouveau sur "Soumettre".  """)
 ui.launch() 
